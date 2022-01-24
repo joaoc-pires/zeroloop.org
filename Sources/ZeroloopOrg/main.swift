1bef47d5c4787abeab4c12d9597a564f31bd940c
Joao Pires <mail@joaopires.dev>
2022-01-24 18:55:02 +0000

    Initial build

---
+++ Sources/ZeroloopOrg/main.swift
@@ -0,0 +1,25 @@
+import Foundation
+import Publish
+import Plot
+
+// This type acts as the configuration for your website.
+struct ZeroloopOrg: Website {
+    enum SectionID: String, WebsiteSectionID {
+        // Add the sections that you want your website to contain here:
+        case posts
+    }
+
+    struct ItemMetadata: WebsiteItemMetadata {
+        // Add any site-specific metadata that you want to use here.
+    }
+
+    // Update these properties to configure your website:
+    var url = URL(string: "https://zeroloop.org")!
+    var name = "ZeroLoop"
+    var description = "Moving, without going anywhere"
+    var language: Language { .english }
+    var imagePath: Path? { nil }
+}
+
+// This will generate your website using the built-in Foundation theme:
+try ZeroloopOrg().publish(withTheme: .foundation)
